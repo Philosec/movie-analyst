f5a6f6fe3c29c51acc580e1222ad5d4425621c95
Shane Padgett <spadgett00@gmail.com>
2018-05-09 20:30:10 -0500

    Modify directory structure

---
+++ movie-analyst-api/server.js
@@ -0,0 +1,9 @@
+// Setup dependancies
+//====================================
+const express = require('express')
+const app = express()
+const jwt = require('express-jwt')
+const rsaValidation = require('auth0-api-jwt-rsa-validation')
+
+// Implement movies api endpoint
+//========================================
